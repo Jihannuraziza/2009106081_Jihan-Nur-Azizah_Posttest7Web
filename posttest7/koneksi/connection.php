c901b090d011ec8dc5b7f7c8f81bcf5c266d147b
Jihannuraziza <102275012+Jihannuraziza@users.noreply.github.com>
2022-11-02 23:38:50 +0800

    Delete posttest7 directory

--- posttest7/koneksi/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	$host = 'localhost'; //nama host
-	$user = 'root'; // nama username dari database
-	$passowrd = ''; //password dari database
-	$db_name = 'laptop_store'; //nama database yang akan dihubungkan
-	
-	$connect = mysqli_connect($host,$user,$passowrd,$db_name);//connect ke database
-?>
